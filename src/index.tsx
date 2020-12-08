ead58eb0b5ab8ea925600704a45fa62b02e4c9ee
Diego Garcia <diego.ds.garcia@gmail.com>
2020-12-08 17:53:23 -0300

    add styled components

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
+import GlobalStyles from './styles/global'
 import routes from './routes'
 
 const App = () => (
   <BrowserRouter>
+    <GlobalStyles />
     <Switch>
       {routes.map((route) => (
         <Route key={route.path} {...route} />
